2529c58867fd3228793574760d2588ad6ab1e4f0
LionyxML <rahul.juliato@gmail.com>
2022-02-20 16:02:58 -0300

    feat: adds scroll when keyboard is rendered over the focused text input

--- reactNative3/src/screens/SignUp/index.tsx
+++ reactNative3/src/screens/SignUp/index.tsx
@@ -15,6 +15,7 @@ import {
   CustomButtomText,
 } from './styles';
 import {useNavigation} from '@react-navigation/native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 export const SignUp = () => {
   const [emailField, setEmailField] = useState('');
@@ -33,28 +34,30 @@ export const SignUp = () => {
       <ZazenLogo width="140px" height="150px" fill="#FFF" />
       <Title>zaZen</Title>
       <InputArea>
-        <SignInput
-          SvgIcon={PersonIcon}
-          placeholder="Digite o seu nome"
-          value={nameField}
-          onChangeText={setNameField}
-        />
-        <SignInput
-          SvgIcon={EmailIcon}
-          placeholder="Digite o seu e-mail"
-          value={emailField}
-          onChangeText={setEmailField}
-        />
-        <SignInput
-          SvgIcon={PassIcon}
-          placeholder="Digite sua senha"
-          value={passwordField}
-          onChangeText={setPasswordField}
-          isPassword
-        />
-        <CustomButton onPress={handleSignInButtonClick}>
-          <CustomButtomText>CADASTRAR</CustomButtomText>
-        </CustomButton>
+        <KeyboardAwareScrollView>
+          <SignInput
+            SvgIcon={PersonIcon}
+            placeholder="Digite o seu nome"
+            value={nameField}
+            onChangeText={setNameField}
+          />
+          <SignInput
+            SvgIcon={EmailIcon}
+            placeholder="Digite o seu e-mail"
+            value={emailField}
+            onChangeText={setEmailField}
+          />
+          <SignInput
+            SvgIcon={PassIcon}
+            placeholder="Digite sua senha"
+            value={passwordField}
+            onChangeText={setPasswordField}
+            isPassword
+          />
+          <CustomButton onPress={handleSignInButtonClick}>
+            <CustomButtomText>CADASTRAR</CustomButtomText>
+          </CustomButton>
+        </KeyboardAwareScrollView>
       </InputArea>
 
       <SignMessageButton onPress={handleMessageButtonClick}>
