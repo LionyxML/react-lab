a7510f04f67b038e83707a408c451876bd0fe1f3
LionyxML <rahul.juliato@gmail.com>
2022-02-19 22:36:38 -0300

    feat: adds base native config with typescript, react-navigation and /stack config, styled components and new sc types lib, a main stack and the Preload stack

--- reactNative2/src/screens/Preload/index.js
+++ reactNative2/src/screens/Preload/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {Container, AppTitle, AppSubTitle} from './styles';
+import SvgUri from 'react-native-svg-uri';
 
-import AppLogo from '../../assets/baldLogo.svg';
+import AppLogo from './baldLogo.svg';
 
 export default () => {
   return (
     <Container>
-      <AppLogo />
+      <SvgUri width="25" height="25" source={require('./baldLogo.svg')} />
+      <AppLogo width="100" height="100" />
       <AppTitle>The Bald App</AppTitle>
       <AppSubTitle>by @</AppSubTitle>
     </Container>
