b87c657375fa8a12dd96f9d2aebde45d58f39070
LionyxML <rahul.juliato@gmail.com>
2022-03-21 21:03:06 -0300

    feat: adds app

--- react-electron/react-electron/public/main.js
+++ react-electron/react-electron/public/main.js
@@ -1,29 +1,42 @@
 const { app, BrowserWindow } = require("electron");
 
-require("@electron/remote/main").initialize(); // IPC (Interprocess Communication)
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+require("@electron/remote/main").initialize();
 
 function createWindow() {
-  // Create the browser window
+  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      enableRemoteModule: true, // IPC (Interprocess Communication) in browser
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
     },
   });
 
-  win.loadURL("http://localhost:3000");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 }
 
 app.on("ready", createWindow);
 
-// For MacOS
-app.on("windo-all-closed", function () {
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
