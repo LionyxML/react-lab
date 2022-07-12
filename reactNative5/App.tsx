769955048643850547eeeaf14bcb6cd22a82f4ce
LionyxML <rahul.juliato@gmail.com>
2022-07-11 23:56:40 -0300

    feat: format configs

--- reactNative5/App.tsx
+++ reactNative5/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
-import WebView from 'react-native-webview';
+import React from "react";
+import { SafeAreaView, StatusBar, useColorScheme, View } from "react-native";
+import WebView from "react-native-webview";
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -14,9 +14,9 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
       <View style={backgroundStyle}>
-        <WebView source={{uri: 'https://tesla.com/'}} />
+        <WebView source={{ uri: "https://auth.protego.com.br/quantum" }} />
       </View>
     </SafeAreaView>
   );
